af2f118259f2a114d0d175c10973b187191c2464
Ospann <115387921+Ospann@users.noreply.github.com>
2023-03-12 11:50:16 +0600

    Update index.php

--- php/index.php
+++ php/index.php
@@ -89,17 +89,20 @@ else if (isset($update['callback_query'])) {
         if ($callback_data === $row) {
             $inline = [];
             foreach ($subProjects as $subarray) {
-                sendMessage($chat_id, $subarray[0] == $callback_data, []);
+                // sendMessage($chat_id, $subarray[0] == $callback_data, []);
                 if ($subarray[0] == $callback_data) {
-                    $inline[] = $subarray[1];
+                    // $inline[] = $subarray[1];
+                    array_push($inline, [
+                        ["text" => $subarray[1], "callback_data" => $subarray[1]]
+                    ]);
                 }
             }
-            // $subprojects_keyboard = array(
-            //     "inline_keyboard" => $inline
-            // );
-            // $text = "Choose subproject:";
-            // sendMessage($chat_id, $text, $subprojects_keyboard);
-            // break;
+            $subprojects_keyboard = array(
+                "inline_keyboard" => $inline
+            );
+            $text = "Choose subproject:";
+            sendMessage($chat_id, $text, $subprojects_keyboard);
+            break;
         }
     }
 }
